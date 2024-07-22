395aa15c60b0a7e7d9c06bf15183a17091c164a0
ssssubin <122021027+ssssubin@users.noreply.github.com>
2024-07-22 16:19:35 +0900

    Time: 298 ms (98.98%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,3 +1,12 @@
 # Write your MySQL query statement below
-select pw.id from Weather as pw join Weather as cw on cw.recordDate = (pw.recordDate-interval 1 day) 
+select pw.id from Weather as pw join Weather as cw on cw.recordDate = (pw.recordDate-interval 1 day)
 where cw.temperature < pw.temperature;
+
+# 현재 날짜 - interval 1 day -> 하루 전 날짜
+# 현재 날짜 + interval 1 day -> 하루 뒤 날짜
+
+# 현재 날짜 - interval 3 month -> 3달 전 날짜
+# 현재 날짜 + interval 3 month -> 3달 후 날짜
+
+# 현재 날짜 - interval 5 year -> 5년 전 날짜
+# 현재 날짜 + interval 5 year -> 5년 후 날짜
