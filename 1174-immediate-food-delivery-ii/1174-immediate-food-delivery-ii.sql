6129bff97c7a7c9e82decdcb746c4cd0d1478bcb
ssssubin <122021027+ssssubin@users.noreply.github.com>
2024-08-05 14:53:25 +0900

    Time: 661 ms (29.28%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,6 +1,12 @@
 # Write your MySQL query statement below
-select round(sum(if(d.order_date = d.delivery_date, 1, 0))/count(customer_id) * 100, 2) as immediate_percentage
-from (select distinct customer_id, 
-first_value(order_date) over (partition by customer_id order by order_date) as order_date, 
-first_value(customer_pref_delivery_date) over (partition by customer_id order by order_date) as delivery_date
- from Delivery) as d;
+# select round(sum(if(d.order_date = d.delivery_date, 1, 0))/count(customer_id) * 100, 2) as immediate_percentage
+# from (select distinct customer_id, 
+# first_value(order_date) over (partition by customer_id order by order_date) as order_date, 
+# first_value(customer_pref_delivery_date) over (partition by customer_id order by order_date) as delivery_date
+#  from Delivery) as d;
+
+select round(sum(if(order_date = customer_pref_delivery_date, 1, 0))/count(customer_id) * 100, 2) as immediate_percentage 
+from Delivery 
+where (customer_id, order_date) 
+in (select customer_id, min(order_date) from Delivery 
+    group by customer_id);
