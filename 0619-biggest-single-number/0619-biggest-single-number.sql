9a0a442972e75c7f512a501dcb114eb1fc360efc
ssssubin <122021027+ssssubin@users.noreply.github.com>
2024-09-25 19:12:30 +0900

    Time: 1638 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,3 +1,2 @@
 # Write your MySQL query statement below
-SELECT nullif(
-    (select num from mynumbers group by num having count(num) =1 order by num desc limit 1), "") as num;
+select ifnull((select num from mynumbers group by num having count(num) =1 order by num desc limit 1), null) as num;
