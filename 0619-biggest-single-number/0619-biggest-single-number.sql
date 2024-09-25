07059add8dd1baea15ddda134f930b8a485e054d
ssssubin <122021027+ssssubin@users.noreply.github.com>
2024-09-25 19:11:17 +0900

    Time: 493 ms (14.21%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-select max((select num from MyNumbers group by num having count(num)=1 order by num desc limit 1)) as num from MyNumbers;
+SELECT nullif(
+    (select num from mynumbers group by num having count(num) =1 order by num desc limit 1), "") as num;
