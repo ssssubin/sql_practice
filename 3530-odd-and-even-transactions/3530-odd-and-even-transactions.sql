06c4326aae75397b0b92990e9f0cfd6c0ce0b033
ssssubin <122021027+ssssubin@users.noreply.github.com>
2025-02-14 09:47:46 +0900

    Time: 299 ms (92.29%), Space: 0B (100%) - LeetHub

--- 3530-odd-and-even-transactions/3530-odd-and-even-transactions.sql
+++ 3530-odd-and-even-transactions/3530-odd-and-even-transactions.sql
@@ -1,17 +1,5 @@
 # Write your MySQL query statement below
-WITH odd AS(
-    SELECT transaction_date, SUM(amount) as odd_sum, 0 as even_sum 
-    FROM transactions 
-    WHERE amount %2 <> 0 
-    GROUP BY transaction_date
-),
-even AS(
-    SELECT transaction_date, 0 as odd_sum, SUM(amount) as even_sum 
-    FROM transactions 
-    WHERE amount %2 = 0 
-    GROUP BY transaction_date
-)
-SELECT transaction_date, SUM(result.odd_sum) as odd_sum, SUM(result.even_sum) as even_sum 
-FROM (SELECT * FROM odd UNION SELECT * FROM even) as result
+SELECT transaction_date, SUM(IF(amount%2 != 0, amount, 0)) AS odd_sum, SUM(IF(amount%2=0, amount, 0)) AS even_sum
+FROM transactions
 GROUP BY transaction_date
-ORDER BY transaction_date;
+ORDER BY transaction_date
